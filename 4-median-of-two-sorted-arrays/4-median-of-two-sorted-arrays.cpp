9c5dca792f755312941f12b019dbd7d88981500b
bhumiii10 <95288854+bhumiii10@users.noreply.github.com>
2022-01-23 03:02:02 +0530

    Time: 71 ms (19.73%), Space: 90.1 MB (19.03%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,74 @@
 class Solution {
+double median(vector<int> v)
+	{
+		sort(v.begin(), v.end());
+		int n = v.size();
+		if(n % 2)
+		{
+		    return v[n/2];
+		}
+		else
+		{
+		    return (v[n/2 - 1] + v[n/2])/2.0;
+		}
+	}
 public:
-double findMedianSortedArrays(vector<int> nums1, vector<int> nums2) {
-
-    int i = 0, j = 0;
-    int m = nums1.size(), n = nums2.size();
-    vector<float> v;
-    
-    while(i < m && j < n)
-    {
-        if(nums1[i] < nums2[j])
-            v.push_back(nums1[i++]);
-        else
-            v.push_back(nums2[j++]);
+    double findMedianSortedArrays(vector<int> nums1, vector<int> nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n == 0 || m == 0)
+        {
+            return ((n == 0) ? median(nums2): median(nums1));
+        }
+        if(m > n) return findMedianSortedArrays(nums2, nums1);
+        int left = 0;
+        int right = m-1;
+        int mid;
+        while(true)
+        {
+            mid = floor(left + (right - left) / 2.0);
+            int other = (n + m)/2 - mid - 2;
+            int left1,left2,right1,right2;
+            if(mid >= 0)
+            left1 = nums2[mid];
+            else
+            left1 = INT_MIN + 2;
+            if(mid+1 < m)
+            right1 = nums2[mid+1];
+            else
+            right1 = INT_MAX - 2;
+            if(other >= 0)
+            left2 = nums1[other];
+            else
+            left2 = INT_MIN + 2;
+            if(other+1 < n)
+            right2 = nums1[other+1];
+            else
+            right2 = INT_MAX - 2;
+            if(left1 <= right2 && left2 <= right1)
+            {
+                if((n + m) % 2)
+                {
+                    return min(right1, right2);
+                }
+                else
+                {
+                    return (max(left1, left2) + min(right1, right2))/2.0;
+                }
+            }
+            else 
+            {
+                if(left1 > right2)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+        }
+        return 0;
     }
-    
-    while(i < m)
-        v.push_back(nums1[i++]);
-    
-    while(j < n)
-        v.push_back(nums2[j++]);
-    
-    int size = v.size();
-    float median = 0;
-    
-    if(size % 2)
-        median = v[size / 2];
-    else
-        median = (v[size / 2 - 1] + v[size / 2]) / 2;
-    
-    return median;
-}
 };
 
